1c9720564c3f4ed6bb96ccee5e8c037183591e98
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-04-04 11:18:12 +0200

    Germany 1995 sample table

--- ioT.R
+++ ioT.R
@@ -2,3 +2,7 @@ require("igraph")
 require("iotables")
 require("ioanalysis")
 
+# sample input-output table
+germany <- iotable_get( source = "germany_1995" )
+germany_iot <- input_flow_get(germany_iot)
+
